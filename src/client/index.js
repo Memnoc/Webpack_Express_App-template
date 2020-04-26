b23ebf54ad4271c31a372ef4b29e80593f3f449d
Memnoc <smartdroidesign@gmail.com>
2020-04-26 09:41:11 +0100

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export {
+    checkForName,
+    handleSubmit
+}
